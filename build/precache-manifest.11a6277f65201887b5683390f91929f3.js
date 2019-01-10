a4d2a76423a5ee81493c98f4a4c3249f63e845f9
itsalle <41296983+itsalle@users.noreply.github.com>
2019-01-10 00:07:53 -0200

    adding build folder

---
+++ build/precache-manifest.11a6277f65201887b5683390f91929f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ccc8d34c6ba53b0ddefa",
+    "url": "/static/js/main.ccc8d34c.chunk.js"
+  },
+  {
+    "revision": "0f258e6db2345b798d5b",
+    "url": "/static/js/1.0f258e6d.chunk.js"
+  },
+  {
+    "revision": "ccc8d34c6ba53b0ddefa",
+    "url": "/static/css/main.3f6a7641.chunk.css"
+  },
+  {
+    "revision": "0f258e6db2345b798d5b",
+    "url": "/static/css/1.cc57b36d.chunk.css"
+  },
+  {
+    "revision": "44d82b0ef57b3442fb2faea57e06b3f7",
+    "url": "/index.html"
+  }
+];
